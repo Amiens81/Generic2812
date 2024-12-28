efc0e4b02816f49a1bd2d299df951e2976c07159
Yuri Blaumanis <39483745+Amiens81@users.noreply.github.com>
2024-12-28 21:41:02 +0300

    Update Main.java

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 //TODO Вторая задача
 public class Main {
 	public static void main(String[] args) {
-       String [] array1 = {"тройка", "семёрка", "туз"};
+           String [] array1 = {"тройка", "семёрка", "туз"};
 	   System.out.println("Изначальный массив: " + String.join(",", array1));
 	   Generic.change(array1, 0, 2);
 	   System.out.println("Массив после обмена позиций двух элементов: " + String.join(",", array1));
 	   Integer [] number1 = {1, 2, 3, 4, 5};
-		System.out.println("Изначальный массив: " + Arrays.toString(number1));
+	   System.out.println("Изначальный массив: " + Arrays.toString(number1));
 	   Generic.change(number1, 1, 4);
 	   System.out.println("Массив после обмена позиций двух элементов: " + Arrays.toString(number1));
 	}
