bd243620e851c3505218977f2fdb059cd45ae5b9
Yuri Blaumanis <39483745+Amiens81@users.noreply.github.com>
2024-12-28 21:57:49 +0300

    Update Generic.java

--- src/Task2/Generic.java
+++ src/Task2/Generic.java
@@ -6,7 +6,7 @@ public class Generic<T> {
 	public static <T> void change(T [] array, int first, int second) {
 
 		if (first < 0 || second < 0 || first >= array.length || second >= array.length) {
-			throw new IllegalArgumentException("Данного индекса в массиве не существует!");
+			throw new ArrayIndexOutOfBoundsException("Данного индекса в массиве не существует!");
 		}
 		T temp = array[first];
 		array[first] = array[second];
