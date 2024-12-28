b9629e9aebc2b273839f50b6859177f65e9ada3f
Yuri Blaumanis <39483745+Amiens81@users.noreply.github.com>
2024-12-28 21:39:32 +0300

    Update Main.java

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -2,11 +2,11 @@ package Task1;
 //TODO Первая зачада
 public class Main {
 	public static void main(String[] args) {
-       String [] array1 = {"регион", "город", "район", "улица" };
+           String [] array1 = {"регион", "город", "район", "улица" };
 	   String [] array2 = {"регион", "город", "район", "улица" };
 	   String [] array3 = {"холодильник", "телевизор", "электропечка"};
 	   String [] array4 = {"регион", "улица", "район", "город" };
-		System.out.println("Сравнение строковых массивов");
+	   System.out.println("Сравнение строковых массивов");
 	   System.out.println("Сравнение 1 и 2 массивов: " + Generic.arrayEqual(array1,array2));
 	   System.out.println("Сравнение 1 и 3 массивов: " + Generic.arrayEqual(array1,array3));
 	   System.out.println("Сравнение 1 и 4 массивов: " + Generic.arrayEqual(array1,array4));
